dc0287017b2bb24d478c7b3315067855860ca1d2
ningmengmao <ge672828794@gmail.com>
2018-07-28 23:14:00 +0800

    create test_project_1

---
+++ src/github_project_1/hello_world.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package github_project_1;
+
+/**
+ * @author ningmengmao
+ *
+ */
+public class hello_world {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("hello world!!!");
+	}
+
+}
