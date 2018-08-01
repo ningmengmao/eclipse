b8966ddf38ff6d05388f8dc3ee98569df7fcfa99
ningmengmao <ge672828794@gmail.com>
2018-08-02 00:13:24 +0800

    递归

---
+++ src/github_project_1/Recursion.java
@@ -0,0 +1,21 @@
+package github_project_1;
+
+class Factorial{
+	long fact(int n) {
+//		int result;
+		if(n == 1)
+			return 1;
+//		result = fact(n-1)*n;
+//		return result;
+		return fact(n-1)*n;
+	}
+}
+
+public class Recursion {
+
+	public static void main(String[] args) {
+	
+		Factorial f = new Factorial();
+		System.out.println(f.fact(20));
+	}
+}
