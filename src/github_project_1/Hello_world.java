4bfa01545beb1f112407aa161cbe7c81a51b6c46
ningmengmao <ge672828794@gmail.com>
2018-08-02 16:43:44 +0800

    8.2

--- src/github_project_1/Hello_world.java
+++ src/github_project_1/Hello_world.java
@@ -38,31 +38,7 @@ class Box {
 	}
 }
 
-class Stack {
-	int stck[] = new int[10];
-	int tos;
-	
-	Stack() {
-		tos = -1;
-	}
-	
-	void push(int item) {
-		if (tos == 9)
-			System.out.println("Stack is full.");
-		else
-			stck[++tos] = item;
-	}
-	
-	int pop() {
-		if (tos < 0) {
-			System.out.println("Stack underflow.");
-			return 0;
-		}
-		else {
-			return stck[tos--];
-		}
-	}
-}
+
 
 class Overload {
 	void test() {
