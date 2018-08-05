1da2a311c41dd9eaff5d131334980d7420663323
ningmeng <ningmeng@XIAOMI-LAPTOP>
2018-08-05 17:04:31 +0800

    day 8.4 win10

---
+++ src/github_project_1/IFTest.java
@@ -0,0 +1,77 @@
+package github_project_1;
+
+class FixedStack implements IntStack {
+	private int stck[];
+	private int tos ;
+	
+	FixedStack(int size) {
+		stck = new int[size];
+		tos = -1;
+	}
+	public void push(int item) {
+		if (tos == stck.length - 1)
+			System.out.println("Stack is full.");
+		else 
+			stck[++tos] = item;
+	}
+	public int pop() {
+		if (tos == -1) {
+			System.out.println("Stack underflow.");
+			return 0;
+		}
+		else 
+			return stck[tos--];
+	}
+}
+
+class DynStack implements IntStack {
+	private int stck[];
+	private int tos;
+	
+	DynStack(int size) {
+		stck = new int[size];
+		tos = -1;
+	}
+	public void push(int item) {
+		if (tos == stck.length - 1) {
+			int temp[] = new int[stck.length * 2];
+			for(int i=0; i<stck.length; i++)
+				temp[i] = stck[i];
+			stck = temp;
+			stck[++tos] = item;
+		}
+		else 
+			stck[++tos] = item;
+	}
+	public int pop() {
+		if (tos == -1) {
+			System.out.println("Stack underflow.");
+			return 0;
+		}
+		else 
+			return stck[tos--];	
+	}
+}
+
+public class IFTest {
+
+	public static void main(String[] args) {
+		IntStack mystack;
+		
+		mystack = new FixedStack(5);
+		for(int i=0; i<5; i++)
+			mystack.push(i);
+		System.out.println("Stack in FixedStack");
+		for(int i=0; i<5; i++)
+			System.out.println(mystack.pop());
+		
+		mystack = new DynStack(8);
+		for(int i=0; i<15; i++)
+			mystack.push(i);
+		System.out.println("Stack in DynStack");
+		for(int i=0; i<15; i++)
+			System.out.println(mystack.pop());
+		
+	}
+
+}
---
+++ src/github_project_1/IntStack.java
@@ -0,0 +1,6 @@
+package github_project_1;
+
+public interface IntStack {
+	void push(int item);
+	int pop();
+}
