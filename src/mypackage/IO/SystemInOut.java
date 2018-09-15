ea14742082869b8df0b9480d7a35056873b67872
ningmengmao <ge672828794@gmail.com>
2018-09-15 15:17:26 +0800

    ...

--- src/mypackage/IO/SystemInOut.java
+++ src/mypackage/IO/SystemInOut.java
@@ -8,7 +8,7 @@ public class SystemInOut {
 	public static void main(String[] args) throws IOException {
 		InputStream ins = System.in;
 		int x = ins.read();
-		ins.close();
+//		ins.close();	System.in可以不关
 		System.out.println(x);
 	}
 
