7eb80f1e4c4476cb2cc8a8665983cc06267bd290
ningmengmao <ge672828794@gmail.com>
2018-09-25 22:18:45 +0800

    添加了深克隆数组的类

---
+++ src/com/ningmeng/util/DeepCopy.java
@@ -0,0 +1,33 @@
+package com.ningmeng.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+/**
+ * 实现数组的深度复制
+ * @author unknown
+ *
+ */
+public class DeepCopy {
+	/**
+	 * 深度复制数组
+	 * @param src 要复制的数组
+	 * @return 返回src的深克隆
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public static <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {
+		ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+		ObjectOutputStream out = new ObjectOutputStream(byteOut);
+		out.writeObject(src);
+		
+		ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(byteOut.toByteArray()));
+		@SuppressWarnings("unchecked")
+		List<T> dest = (List<T>) in.readObject();
+		return dest;
+	}
+}
--- src/mypackage/xml/Dom4jTest.java
+++ src/mypackage/xml/Dom4jTest.java
@@ -1,17 +1,25 @@
 package mypackage.xml;
 
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 
 
 public class Dom4jTest {
 
-	public static void main(String[] args) throws DocumentException {
+	public static void main(String[] args) throws Exception {
 //		test0();
+//		test1();
+		test2();
 	}
 
 	public static void test0() throws DocumentException {
@@ -25,5 +33,35 @@ public class Dom4jTest {
 			//p1下的name标签的文本值
 			 System.out.println(e.element("name").getText());
 		}
+	}
+	public static void test1() throws DocumentException, IOException {
+		SAXReader reader = new SAXReader();
+		Document doc = reader.read("src/mypackage/xml/1.xml");
+		Element root = doc.getRootElement();
+		//第一个p1
+		Element p1 = root.element("p1");
+		Element sex = p1.addElement("sex");
+		sex.addText("nan");
+		//写回XML
+		//OutputFormat.createPrettyPrint() 漂亮的格式(带回车,空格的)
+		//OutputFormat.createCompactFormat() 压缩的格式(去掉空格 回车)
+		XMLWriter w = new XMLWriter(new FileOutputStream("src/mypackage/xml/1.xml"), 
+				OutputFormat.createPrettyPrint()) ;  
+		w.write(doc);
+		w.close();		
+	}
+	public static void test2() throws Exception {
+		SAXReader reader = new SAXReader();
+		Document doc = reader.read("src/mypackage/xml/1.xml");
+		Element root = doc.getRootElement();
+		Element p1 = root.element("p1");
+		//新建元素
+		Element school = DocumentHelper.createElement("school");
+		school.addText("ecit");
+		p1.elements().add(1, school);
+		XMLWriter w = new XMLWriter(new FileOutputStream("src/mypackage/xml/1.xml"),
+				OutputFormat.createPrettyPrint());
+		w.write(doc);
+		w.close();
 	}
 }
